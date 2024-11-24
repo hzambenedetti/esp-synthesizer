07055d3aa01e5fce3dd406f6eb92b907585f6bb2
hzambenedetti <hique.rz03@gmail.com>
2024-11-24 19:42:55 -0300

    add: Updating main and wave files to the latest versions

--- rusty_synth/src/main.rs
+++ rusty_synth/src/main.rs
@@ -1,115 +1,196 @@
-//! This shows how to use the TIMG peripheral interrupts.
 //!
-//! There is TIMG0 which contains a general purpose timer and a watchdog timer.
+//! Without an additional I2S sink device you can inspect the MCLK, BCLK, WS
+//!  and DOUT with a logic analyzer.
+//!
+//! You can also connect e.g. a PCM510x to hear an annoying loud sine tone (full
+//! scale), so turn down the volume before running this example.
+//!
 
-//% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+//! - BCLK => GPIO2
+//! - WS   => GPIO4
+//! - DOUT => GPIO5
+//!
+//! PCM510x:
+//! | Pin   | Connected to    |
+//! |-------|-----------------|
+//! | BCK   | GPIO0           |
+//! | DIN   | GPIO2           |
+//! | LRCK  | GPIO1           |
+//! | SCK   | Gnd             |
+//! | GND   | Gnd             |
+//! | VIN   | +3V3            |
+//! | FLT   | Gnd             |
+//! | FMT   | Gnd             |
+//! | DEMP  | Gnd             |
+//! | XSMT  | +3V3            |
+
+//% CHIPS: esp32 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
 
 #![no_std]
 #![no_main]
-
-
+#[allow(unused)]
 use esp_backtrace as _;
 use esp_hal::{
-    dma::{Dma, DmaPriority},  dma_circular_buffers, gpio::{Io, Level, Output}, i2s::{DataFormat, I2s, I2sTx, I2sWrite, I2sWriteDma, Standard}, peripherals::I2S0, prelude::*, Blocking
+    analog::adc::{
+        Adc,
+        AdcConfig,
+        AdcCalLine,
+        AdcCalBasic,
+        AdcCalCurve,
+        Attenuation,
+    }
+    , dma::{
+        Dma, 
+        DmaPriority
+    },  
+    dma_circular_buffers, 
+    gpio::{
+        Io,
+    }, 
+    i2s::{
+        DataFormat, 
+        I2s, 
+        I2sTx, 
+        I2sWriteDma, 
+        Standard
+    }, peripherals::I2S0, peripherals::ADC1, prelude::*, time, xtensa_lx::timer::delay, Blocking
 };
-use wave::sin_i16_bytes;
+
+use crate::wave::constants::SINE;
 
 mod wave;
+mod oscilator;
 
-use crate::wave::{
-    sin_i16,
-    constants::{
-        FULL_WAVE,
-        SINE
-    }
-};
-
-static mut PHASE: f32 = 0.0;
-
-
-const TX_BUFF_LEN: usize = 4096;
-// const STEP: f32 = (60.0 * FULL_WAVE as f32)/44100.0;
-const STEP: f32 = 1.0;
-
+const TX_BUFFER_SIZE: usize = 4096;
+const STEP: f32 = 1.0;//(60.0 * 1024.0)/44100.0;
+const STEP_DIV: f32 = SINE.len() as f32/44100.0;
+const FREQ_DIV: f32 = 400.0/256.0;
+// Se STEP aumenta, a frequencia aumenta tambem
+// Se o número de amostras no seno aumenta a frequencia diminui
 
 #[entry]
 fn main() -> ! {
     
-    let mut sys_cfg = esp_hal::Config::default();
-    sys_cfg.cpu_clock = CpuClock::max();
+    // esp_println::logger::init_logger_from_env();
+    // let delay = Delay::new();
 
     let peripherals = esp_hal::init(esp_hal::Config::default());
 
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
     
+    //ADC setup
+    let mut adc_config: AdcConfig<ADC1> = AdcConfig::new();
+    let mut adc_pin = adc_config.enable_pin_with_cal::<_, AdcCalLine<_>>( 
+        io.pins.gpio3,
+        Attenuation::Attenuation11dB
+    );
+
+
+    let mut adc_driver = Adc::new(
+        peripherals.ADC1,
+        adc_config
+    );
+
     
-    
-    //DMA init 
+    //I2S and DMA setup
     let dma = Dma::new(peripherals.DMA);
     let dma_channel = dma.channel0;
-    
-    let (_, rx_desc, tx_buff, tx_desc) = dma_circular_buffers!(0, TX_BUFF_LEN);
-    //I2s init
+
+    let (_, rx_descriptors, tx_buffer, tx_descriptors) = dma_circular_buffers!(0, TX_BUFFER_SIZE);
+
     let i2s = I2s::new(
         peripherals.I2S0,
         Standard::Philips,
         DataFormat::Data16Channel16,
         44100.Hz(),
         dma_channel.configure(false, DmaPriority::Priority0),
-        rx_desc,
-        tx_desc,
+        rx_descriptors,
+        tx_descriptors,
     );
 
-
-
-    let mut i2s_tx: I2sTx<I2S0, Blocking> = i2s 
+    let mut i2s_tx: I2sTx<I2S0,Blocking> = i2s
         .i2s_tx
-        .with_bclk(io.pins.gpio15)
-        .with_ws(io.pins.gpio4)
-        .with_dout(io.pins.gpio5)
+        .with_bclk(io.pins.gpio4)
+        .with_ws(io.pins.gpio5)
+        .with_dout(io.pins.gpio15)
         .build();
+
+
+
+
+    let data = unsafe { 
+        core::slice::from_raw_parts(&SINE as *const _ as *const u8, SINE.len() * 2) 
+    };
     
+    let mut step = 60.0 * STEP_DIV;
+    let mut j = 0.0;
+    for i in (0..tx_buffer.len()).step_by(2){
+        let k = (j as usize) << 1;
+        tx_buffer[i] = data[k];
+        tx_buffer[i+1] = data[k + 1];
+        j += step;
+
+        if j >= (data.len()/2) as f32{ j = 0.0;}
+
+    }
+
+
+    let mut filler = [0u8; TX_BUFFER_SIZE];
+    let mut transfer = i2s_tx.write_dma_circular(&tx_buffer).unwrap();
     
-    //STEP = (200.0 * 80)/44100
-    let mut phase: f32 = 0.0;
-    let mut j = 0;
-    for _ in 0..TX_BUFF_LEN/2{
-        let bytes = sin_i16(j).to_ne_bytes();
-        tx_buff[j] = bytes[0];
-        tx_buff[j+1] = bytes[1];
-        
-        phase += STEP;
-        j+= 2;
-        if phase > SINE.len() as f32{
-            phase = 0.0;
+
+    let mut idx = 0.0;
+    let transfer_size = transfer.available();
+    for i in (0..transfer_size).step_by(2){
+        let k = (idx as usize) << 1;
+        filler[i] = data[k];
+        filler[i+1] = data[k + 1];
+
+        idx += step;
+        if idx > (data.len()/2) as f32{
+            idx = 0.0;
         }
     }
 
-    let mut transfer = i2s_tx.write_dma_circular(&tx_buff).unwrap();
+    transfer.push(&filler[0..transfer_size]).unwrap();
     
-    let mut filler: [u8; TX_BUFF_LEN] = [0; TX_BUFF_LEN];
+    let mut freq = adc_driver.read_blocking(&mut adc_pin) >> 4;
+    let mut adc_counter: u32 = 0; 
     loop {
-        if transfer.available() > 1{
-            let mut size = usize::min(transfer.available(), TX_BUFF_LEN);
-            if size & 1 != 0 {size -= 1;}
-            fill_transfer_buffer(&mut filler, size, &mut phase);
-            transfer.push(&filler[..size]).unwrap();
-        }
-    }
-}
+        adc_counter += 1;
+        let avail = transfer.available();
+        if avail > 0 {
+            let avail = usize::min(10000, avail);
+            for bidx in (0..avail).step_by(2) {
+                // let k = (idx as usize) << 1;
+                let bytes = SINE[idx as usize].to_ne_bytes();
+                filler[bidx] = bytes[0];
+                filler[bidx + 1] = bytes[1];
+                idx += step;
 
-fn fill_transfer_buffer(buffer: &mut[u8],size: usize, phase: &mut f32){
-    let mut j = 0;
-    while j < size{
-        let bytes = sin_i16_bytes(*phase as usize); 
-        buffer[j] = bytes.0;
-        buffer[j+1] = bytes.1;
-        j += 2;
+                if idx >= (data.len()/2) as f32{
+                    idx = 0.0;
+                }
+            }
+            transfer.push(&filler[0..avail]).unwrap();
+        }
         
-        *phase += STEP;
-        if *phase > SINE.len() as f32{
-            *phase = 0.0;
+        if adc_counter > 100_000{
+           adc_counter = 0;
+            let adc_read = adc_driver.read_blocking(&mut adc_pin) >> 4;
+            let delta = abs(adc_read as i16 - freq as i16); 
+                if delta > 7{
+                    freq = adc_read;
+                    step = (freq as f32) * FREQ_DIV * STEP_DIV;
+                }
+            // esp_println::println!("{freq}");
         }
+        
     }
 }
 
+
+fn abs(x: i16) -> i16{
+    if x < 0 {return -x;}
+    return x;
+}
