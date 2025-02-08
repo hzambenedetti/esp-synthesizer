a9041d39ecc75d870d2a16b3d14d1e595a9df4c7
Henrique Bubniak <hg.bubniak@gmail.com>
2025-02-08 18:59:18 -0300

    feat: increased window size in moving average for adc reads

--- rusty_synth/src/analog_reader.rs
+++ rusty_synth/src/analog_reader.rs
@@ -27,7 +27,7 @@ where
     pub fn read(&mut self, driver: &mut Adc<ADC1>) -> f32 {
         let value = driver.read_blocking(&mut self.adc_pin);
         //exponential moving average (smoothing / (window_size + 1))
-        let alpha = 2.0 / 65.0;
+        let alpha = 2.0 / 257.0;
         self.accumulator =
             ((alpha * value as f32) + (1.0 - alpha) * self.accumulator as f32) as u16;
         (self.activation)(self.accumulator)
